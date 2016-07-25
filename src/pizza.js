ee004cb3d2b1cfb90faa8d2adea8757159049ec4
sweetkeys <shaun@shaunsweet.com>
2016-07-24 23:05:54 -0700

    removed uneeded code

--- src/pizza.js
+++ src/pizza.js
@@ -5,7 +5,7 @@ class Pizza extends Component {
 	render(){
 		return (
 			<div id={"pizza"+this.props.id} key={this.props.id}>
-				<a href="#" onClick={()=> this.props.onClick(this.props.toppings)} data-toggle="modal" data-target="#pizzaModal">{this.props.name}</a>
+				<a href="#" onClick={()=> this.props.onClick(this.props.toppings)}>{this.props.name}</a>
 			</div>
 			);
 	}
